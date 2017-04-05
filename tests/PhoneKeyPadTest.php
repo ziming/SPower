d3c8c5c5b464894c8ce91dc8ada6e7cde3427083
Ziming Lan <ziming.lan@gmail.com>
2017-04-05 14:02:18 +0800

    q4 test

--- tests/PhoneKeyPadTest.php
+++ tests/PhoneKeyPadTest.php
@@ -50,6 +50,22 @@ class PhoneKeyPadTest extends TestCase
         $this->assertSimilarArrays($expectedResult, $letterCombinations);
     }
 
+    /** @test */
+    function can_return_all_possible_word_combinations_from_dictionary() {
+
+        // Arrange
+        $phoneKeyPad = new PhoneKeyPad;
+
+        // Act
+        $wordArray1 = $phoneKeyPad->getAllDictionaryWordsFromNumber(2355);
+        $wordArray2 = $phoneKeyPad->getAllDictionaryWordsFromNumber(4663);
+
+        // Assert
+        $this->assertSimilarArrays(['bell', 'cell'], $wordArray1);
+        $this->assertSimilarArrays(['good', 'goof', 'gone', 'home', 'hone', 'hood', 'hoof'], $wordArray2);
+
+    }
+
     private function assertSimilarArrays($expectedArray, $actualArray) {
 
         // https://stackoverflow.com/questions/3838288/phpunit-assert-two-arrays-are-equal-but-order-of-elements-not-important

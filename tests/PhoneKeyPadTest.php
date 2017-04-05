3b46c966c30de04c0c6ecd39d6d84e9b37520e87
Ziming Lan <ziming.lan@gmail.com>
2017-04-05 13:37:15 +0800

    1st test for qn 3 generate letter combinations from number

--- tests/PhoneKeyPadTest.php
+++ tests/PhoneKeyPadTest.php
@@ -35,4 +35,19 @@ class PhoneKeyPadTest extends TestCase
 
     }
 
+    /** @test */
+    function can_return_all_possible_letter_combinations_number_could_represent() {
+
+        // Arrange
+        $phoneKeyPad = new PhoneKeyPad;
+
+        // Act
+        $letterCombinations = $phoneKeyPad->getAllLetterCombinationsFromNumber(23);
+
+        // Assert
+        $expectedResult = ['ad', 'ae', 'af', 'bd', 'be', 'bf', 'cd', 'ce', 'cf'];
+
+        $this->assertSimilarArrays($expectedResult, $letterCombinations);
+    }
+
 }
